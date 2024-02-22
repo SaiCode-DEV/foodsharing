68a62536d14c27111599391f5465416c1b3830c5
Anton Ballmaier <aballmaier@posteo.de>
2024-02-22 11:35:05 +0100

    save regex

--- client/src/components/Markdown/markdownRenderer.js
+++ client/src/components/Markdown/markdownRenderer.js
@@ -33,15 +33,16 @@ md.linkify.data = {
 }
 
 md.linkify.add('@', {
-  validate: function (text, pos) {
+  validate: function (text, pos, self) {
+    self.re.profileId ??= /^\d{3,}/
     const tail = text.slice(pos)
-    if (/^\d+/.test(tail)) {
-      return tail.match(/\d+/)[0].length
+    if (self.re.profileId.test(tail)) {
+      return tail.match(self.re.profileId)[0].length
     }
     return false
   },
   async normalize (match, self) {
-    const id = match.url.slice(1)
+    const id = +match.url.slice(1)
     match.url = '/profile/' + id
     if (self.data.userNames[id]) {
       match.text = '@' + self.data.userNames[id]
