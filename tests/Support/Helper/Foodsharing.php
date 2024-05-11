e22636ccb9df74838137a0a5d4aa8724cf70d790
Alex <alexander.simm@posteo.de>
2024-05-11 14:47:15 +0000

    speed up tests

--- tests/Support/Helper/Foodsharing.php
+++ tests/Support/Helper/Foodsharing.php
@@ -620,7 +620,7 @@ class Foodsharing extends Db
 
         // add up to 10 emails to each folder
         foreach ([MailboxFolder::FOLDER_INBOX, MailboxFolder::FOLDER_SENT, MailboxFolder::FOLDER_TRASH] as $folder) {
-            $numMails = $this->faker->numberBetween(20, 150);
+            $numMails = $this->faker->numberBetween(10, 20);
             for ($i = 0; $i < $numMails; ++$i) {
                 $this->createEmail($mb, $folder);
             }
