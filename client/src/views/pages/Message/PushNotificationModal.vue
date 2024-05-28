d1c1b9306673084cec6fd280a19a6d5b78c55281
Anton Ballmaier <aballmaier@posteo.de>
2024-05-28 14:51:49 +0200

    add PushNotificationModal

---
+++ client/src/views/pages/Message/PushNotificationModal.vue
@@ -0,0 +1,30 @@
+<template>
+  <b-modal
+    ref="modal"
+    centered
+    title="Push-Benachrichtigungen erhalten?"
+    ok-title="Push-Benachrichtigungen erhalten"
+    cancel-title="nicht aktivieren"
+    :cancel-variant="dontAskAgain ? 'outline-danger' : ''"
+  >
+    Damit du schneller mitbekommst, wenn andere Foodsaver:innen dir Nachrichten schicken, kannst du auf diesem Gerät Push-Benachrichtigungen aktivieren!
+    <hr>
+    In deinen <a href="#">Benachichtigungseinstellungen</a> kannst du diese Benachrichtigungen jeder Zeit wieder abschalten.
+    <hr>
+    <b-form-checkbox v-model="dontAskAgain">
+      Nicht wieder nachfragen
+    </b-form-checkbox>
+  </b-modal>
+</template>
+<script>
+export default {
+  data: () => ({
+    dontAskAgain: false,
+  }),
+  methods: {
+    maybeShow () {
+      this.$refs.modal.show()
+    },
+  },
+}
+</script>
