36e716f71857a68a940c2dde94b760d0018694b2
Anton Ballmaier <aballmaier@posteo.de>
2024-07-02 01:14:55 +0200

    fix test

--- tests/Api/CalendarApiCest.php
+++ tests/Api/CalendarApiCest.php
@@ -147,7 +147,7 @@ class CalendarApiCest
         $I->seeResponseContains(substr((string)$this->acceptedEvent['name'], 0, 10));
         $I->seeResponseContains(substr((string)$this->invitedEvent['name'], 0, 10));
 
-        $I->sendGet('api/calendar/' . self::TEST_TOKEN . '?events=answered');
+        $I->sendGet('api/calendar/' . self::TEST_TOKEN . '?events=maybe');
         $I->seeResponseCodeIs(HttpCode::OK);
         $I->seeResponseContains(substr((string)$this->acceptedEvent['name'], 0, 10));
         $I->cantSeeResponseContains(substr((string)$this->invitedEvent['name'], 0, 10));
