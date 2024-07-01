2b0978bb39064b740dbe0c103ffb92b7316bc342
Anton Ballmaier <aballmaier@posteo.de>
2024-07-01 11:04:17 +0200

    Rework Calendar Frontend

--- src/Modules/Settings/components/Calendar.vue
+++ src/Modules/Settings/components/Calendar.vue
@@ -1,84 +1,90 @@
 <template>
   <div class="settings-calendar">
-    <h3 class="heading">
-      <i class="fas fa-fw fa-lightbulb" />
-      {{ $i18n('settings.calendar.link_title') }}
-    </h3>
-    <div class="bootstrap">
-      <p> {{ $i18n('settings.calendar.teaser') }} <a :href="$url('wiki_calendar')" target="_blank">{{ $url('wiki_calendar') }}</a></p>
-      <b-button
-        class="my-2"
-        @click="createToken"
+    <b-alert variant="info" show>
+      <Markdown :source="$i18n('settings.calendar.info', { calendarUrl: $url('wiki_calendar') })" />
+    </b-alert>
+    <b-form>
+      <b-form-group :label="$i18n('settings.calendar.pickup.label')">
+        <b-form-select v-model="includePickups" :options="pickupOptions" />
+      </b-form-group>
+      <b-form-group :label="$i18n('settings.calendar.event.label')">
+        <b-form-select v-model="includeEvents" :options="eventOptions" />
+      </b-form-group>
+      <b-form-group :label="$i18n('settings.calendar.history.label')">
+        <b-form-select v-model="includeHistory" :options="historyOptions" />
+      </b-form-group>
+      <b-form-group :label="$i18n('settings.calendar.program.label')">
+        <b-form-select v-model="selectedProgram" :options="programOptions" />
+      </b-form-group>
+      <div v-if="selectedProgram === 'other'" class="ml-4">
+        <b-form-group :label="$i18n('settings.calendar.protocol')">
+          <b-form-select v-model="protocol" :options="protocolOptions" />
+        </b-form-group>
+        <b-form-group :label="$i18n('settings.calendar.formatting.label')">
+          <b-form-select v-model="formatting" :options="formattingOptions" />
+        </b-form-group>
+      </div>
+    </b-form>
+    <b-button-toolbar>
+      <b-dropdown
+        split
+        variant="success"
+        right
+        :text="$i18n('settings.calendar.generate_url.button')"
+        :disabled="!selectedProgram"
+        @click="copyUrl"
       >
-        {{ $i18n('settings.calendar.create_token.button') }}
-      </b-button>
+        <b-dropdown-item @click="download">
+          {{ $i18n('settings.calendar.download') }}
+        </b-dropdown-item>
+      </b-dropdown>
       <b-button
         v-if="token"
-        class="my-2"
+        variant="outline-danger"
         @click="removeToken"
       >
         {{ $i18n('settings.calendar.delete_token.button') }}
       </b-button>
-
-      <div v-if="token" class="mt-3">
-        <hr>
-
-        <b-form-checkbox v-model="includeInvitations" class="mt-3">
-          {{ $i18n('settings.calendar.include_invitations') }}
-        </b-form-checkbox>
-
-        <ul class="webcal">
-          <li class="pb-1">
-            <a :href="webcalPickups">
-              {{ webcalPickups }}
-            </a>
-          </li>
-          <li class="pb-1">
-            <a :href="httpPickups">
-              {{ httpPickups }}
-            </a>
-          </li>
-        </ul>
-      </div>
-
-      <b-alert variant="warning" show>
-        {{ $i18n('settings.calendar.token-warning') }}
-      </b-alert>
-
-      <b-alert variant="secondary" show>
-        {{ $i18n('settings.calendar.sync') }}
-      </b-alert>
-    </div>
+    </b-button-toolbar>
   </div>
 </template>
-
 <script>
 import { hideLoader, pulseError, showLoader } from '@/script'
 import { createApiToken, getApiToken, removeApiToken } from '@/api/calendar'
-import i18n from '@/helper/i18n'
 import ConfirmationDialogue from '@/mixins/ConfirmationDialogue'
+import Markdown from '@/components/Markdown/Markdown.vue'
 
 export default {
+  components: { Markdown },
   mixins: [ConfirmationDialogue],
-  props: {
-    baseUrlWebcal: { type: String, required: true },
-    baseUrlHttp: { type: String, required: true },
-  },
   data () {
     return {
       token: null,
-      includeInvitations: true,
+
+      includePickups: true,
+      includeEvents: 'invitations',
+      includeHistory: true,
+      selectedProgram: null,
+      protocol: 'http',
+      formatting: 'alt',
+      pickupOptions: this.optionsArray([true, false], 'pickup'),
+      eventOptions: this.optionsArray(['all', 'invitations', 'maybe', 'accepted', 'none'], 'event'),
+      historyOptions: this.optionsArray([true, false], 'history'),
+      programOptions: [
+        { value: { protocol: 'http', formatting: 'html' }, text: this.$i18n('settings.calendar.program.google') },
+        { value: { protocol: 'webcal', formatting: 'alt' }, text: this.$i18n('settings.calendar.program.outlook') },
+        { value: { protocol: 'http', formatting: 'alt' }, text: this.$i18n('settings.calendar.program.thunderbird') },
+        { value: 'other', text: this.$i18n('settings.calendar.program.other') },
+      ],
+      protocolOptions: ['http', 'webcal'].map(value => ({ value, text: `${value}://...` })),
+      formattingOptions: this.optionsArray(['html', 'alt', 'text'], 'formatting'),
     }
   },
   computed: {
-    queryParams () {
-      return `?events=${this.includeInvitations ? 'all' : 'answered'}`
-    },
-    webcalPickups () {
-      return this.baseUrlWebcal + this.token + this.queryParams
-    },
-    httpPickups () {
-      return this.baseUrlHttp + this.token + this.queryParams
+    url () {
+      const programSettings = this.selectedProgram === 'other' ? this : this.selectedProgram
+      if (!programSettings || !this.token) return false
+      return `${programSettings.protocol}://${location.host}/api/calendar/${this.token}?formatting=${programSettings.formatting}&events=${this.includeEvents}&pickups=${this.includePickups}&history=${this.includeHistory}`
     },
   },
   async mounted () {
@@ -91,17 +97,30 @@ export default {
     hideLoader()
   },
   methods: {
-    async createToken () {
-      if (this.token) {
-        if (!await this.confirmationDialogue('settings.calendar.create_token.message', { okTitle: i18n('yes') })) return
+    optionsArray (keys, type) {
+      return keys.map(key => ({ value: key, text: this.$i18n(`settings.calendar.${type}.${key}`) }))
+    },
+    async haveToken () {
+      if (!this.token) {
+        showLoader()
+        try {
+          this.token = await createApiToken()
+        } catch (e) {
+          pulseError(this.$i18n('error_unexpected'))
+          return
+        }
+        hideLoader()
       }
-      showLoader()
-      try {
-        this.token = await createApiToken()
-      } catch (e) {
-        pulseError(i18n('error_unexpected'))
-      }
-      hideLoader()
+    },
+    async copyUrl () {
+      await this.haveToken()
+      console.debug(this.url)
+    },
+    async download () {
+      await this.haveToken()
+      const link = document.createElement('a')
+      link.href = this.url.replace('webcal', 'http') // always use http for download
+      link.click()
     },
     async removeToken () {
       if (!await this.confirmationDialogue('settings.calendar.delete_token.message')) return
@@ -110,34 +129,24 @@ export default {
         await removeApiToken()
         this.token = null
       } catch (e) {
-        pulseError(i18n('error_unexpected'))
+        pulseError(this.$i18n('error_unexpected'))
       }
       hideLoader()
     },
   },
 }
 </script>
-
-<style lang="scss" scoped>
-.heading {
-  padding: 0.2rem 0.1rem;
-  font-family: 'Alfa Slab One', serif;
-  font-weight: normal;
-  font-size: 1rem;
-  color: var(--fs-color-primary-500);
-}
-
-.webcal {
-  font-weight: bolder;
-
-  // --breakpoint-xs
-  @media (max-width: 576px) {
-    word-break: break-all;
-    overflow-wrap: break-word;
+<style scoped lang="scss">
+::v-deep.btn-toolbar {
+  gap: 0.5em;
+  > * {
+    margin-bottom: 0.5em;
   }
-
-  li a {
-    color: var(--fs-color-secondary-500);
+  .dropdown:first-child {
+    flex-grow: 1;
+    > .dropdown-toggle-split {
+      flex-grow: 0;
+    }
   }
 }
 </style>
