fcf91fb809560834b7d9399b38439a5147d76063
Christian Walgenbach <foodsharing@walgeo.de>
2024-08-09 14:27:21 +0000

    added landline in error container and moved to computed

--- client/src/components/Banners/Errors/ErrorContainer.vue
+++ client/src/components/Banners/Errors/ErrorContainer.vue
@@ -22,108 +22,99 @@ export default {
     ErrorField,
     ModalLoader, // is required because the website is not a single vue instance (each module has its own instance)  and otherwise the modals are only over datastore accessable and not by the global $bvmodal function
   },
-  data () {
-    return {
-      list: [],
-    }
-  },
-  async mounted () {
-    /**
-     * Shows an error when the user has an invalid mobile phone number
-     */
-    await DataUser.mutations.fetchDetails()
-    const mobilePhoneNumber = DataUser.getters.getMobilePhoneNumber()
-    if (mobilePhoneNumber && !isValidPhoneNumber(mobilePhoneNumber)) {
-      this.list.push({
-        field: 'invalid_mobile_phonenumber',
-        links: [{
-          text: 'error.invalid_mobile_phonenumber.link',
-          urlShortHand: 'settings',
-        }],
-      })
-    }
-    /**
-     * Shows an error when the user has no avatar
-     */
-    if (DataUser.getters.getAvatar() === null) {
-      this.list.push({
-        field: 'missing_user_avatar',
-        links: [{
-          text: 'error.missing_user_avatar.link',
-          urlShortHand: 'settings',
-        }],
-      })
-    }
-    /**
-     * Shows an error when the user has an old avatar
-     */
-    if (DataUser.getters.getAvatar() && !DataUser.getters.getAvatar().startsWith('/api/uploads/')) {
-      this.list.push({
-        field: 'old_user_avatar',
-        link: 'images/' + DataUser.getters.getAvatar(),
-        links: [{
-          text: 'error.old_user_avatar.link',
-          urlShortHand: 'settings',
-        }],
-      })
-    }
-    /**
-     * Shows an error when the user has no home region
-     */
-    if (DataUser.getters.isFoodsaver() && !DataUser.getters.hasHomeRegion()) {
-      this.$bvModal.show('joinRegionModal')
-      this.list.push({
-        field: 'missing_home_region',
-        links: [{
-          text: 'error.missing_home_region.link',
-          modal: 'joinRegionModal',
-        }],
-      })
-    }
-    /**
-     * Shows an error when the user has no valid geolocation
-     */
-    if (DataUser.getters.isFoodsaver() && !DataUser.getters.hasLocations()) {
-      this.list.push({
-        field: 'missing_geolocation',
-        links: [{
-          text: 'error.missing_geolocation.link',
-          urlShortHand: 'settings',
-        }],
-      })
-    }
-    /**
-     * Shows an error when the user needs to activate his email
-     */
-    if (!DataUser.getters.hasActiveEmail()) {
-      this.list.push({
-        field: 'mail_activation',
-        links: [{
-          text: 'error.mail_activation.link_1',
-          urlShortHand: 'resendActivationMail',
-        },
-        {
-          text: 'error.mail_activation.link_2',
-          urlShortHand: 'settings',
-        }],
-      })
-    }
-    /**
-     * Shows an error when the user has a bouncing email
-     */
-    if (DataUser.getters.hasBouncingEmail()) {
-      this.list.push({
-        field: 'mail_bounce',
-        links: [{
-          text: 'error.mail_bounce.link_1',
-          urlShortHand: 'settings',
-        },
-        {
-          text: 'error.mail_bounce.link_2',
-          urlShortHand: 'freshdesk_locked_email',
-        }],
-      })
-    }
+  computed: {
+    list () {
+      const list = []
+
+      DataUser.mutations.fetchDetails()
+
+      const mobilePhoneNumber = DataUser.getters.getMobilePhoneNumber()
+      if (mobilePhoneNumber && !isValidPhoneNumber(mobilePhoneNumber)) {
+        list.push({
+          field: 'invalid_mobile_phonenumber',
+          links: [{
+            text: 'error.invalid_mobile_phonenumber.link',
+            urlShortHand: 'settings',
+          }],
+        })
+      }
+
+      const landlinePhoneNumber = DataUser.getters.getPhoneNumber()
+      if (landlinePhoneNumber && !isValidPhoneNumber(landlinePhoneNumber)) {
+        list.push({
+          field: 'invalid_landline_phonenumber',
+          links: [{
+            text: 'error.invalid_landline_phonenumber.link',
+            urlShortHand: 'settings',
+          }],
+        })
+      }
+
+      if (DataUser.getters.getAvatar() === null) {
+        list.push({
+          field: 'missing_user_avatar',
+          links: [{
+            text: 'error.missing_user_avatar.link',
+            urlShortHand: 'settings',
+          }],
+        })
+      }
+
+      if (DataUser.getters.getAvatar() && !DataUser.getters.getAvatar().startsWith('/api/uploads/')) {
+        list.push({
+          field: 'old_user_avatar',
+          link: 'images/' + DataUser.getters.getAvatar(),
+          links: [{
+            text: 'error.old_user_avatar.link',
+            urlShortHand: 'settings',
+          }],
+        })
+      }
+
+      if (DataUser.getters.isFoodsaver() && !DataUser.getters.hasHomeRegion()) {
+        this.$bvModal.show('joinRegionModal')
+      }
+
+      if (DataUser.getters.isFoodsaver() && !DataUser.getters.hasLocations()) {
+        list.push({
+          field: 'missing_geolocation',
+          links: [{
+            text: 'error.missing_geolocation.link',
+            urlShortHand: 'settings',
+          }],
+        })
+      }
+
+      if (!DataUser.getters.hasActiveEmail()) {
+        list.push({
+          field: 'mail_activation',
+          links: [{
+            text: 'error.mail_activation.link_1',
+            urlShortHand: 'resendActivationMail',
+          },
+          {
+            text: 'error.mail_activation.link_2',
+            urlShortHand: 'settings',
+          }],
+        })
+      }
+
+      if (DataUser.getters.hasBouncingEmail()) {
+        list.push({
+          field: 'mail_bounce',
+          links: [{
+            text: 'error.mail_bounce.link_1',
+            urlShortHand: 'settings',
+          },
+          {
+            text: 'error.mail_bounce.link_2',
+            urlShortHand: 'freshdesk_locked_email',
+          }],
+        })
+      }
+
+      return list
+    },
   },
 }
 </script>
