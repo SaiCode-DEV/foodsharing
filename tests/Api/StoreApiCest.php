0013e8168755ecf9c7ec8a06bec7d28ca3f42d57
Anton Ballmaier <aballmaier@posteo.de>
2024-08-27 16:44:35 +0200

    fix code style

--- tests/Api/StoreApiCest.php
+++ tests/Api/StoreApiCest.php
@@ -7,8 +7,6 @@ namespace Tests\Api;
 use Codeception\Example;
 use Codeception\Util\HttpCode as Http;
 use Faker\Factory;
-use Foodsharing\Modules\Core\DBConstants\Store\Milestone;
-use Foodsharing\Modules\Core\DBConstants\Store\StoreLogAction;
 use Foodsharing\Modules\Core\DBConstants\Unit\UnitType;
 use Tests\Support\ApiTester;
 
