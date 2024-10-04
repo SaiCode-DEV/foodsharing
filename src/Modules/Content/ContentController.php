32dae74faba0a65edef5c66e943d55a325f98a37
Christian Walgenbach <foodsharing@walgeo.de>
2024-10-04 20:30:23 +0000

    update release info for 2024-12

--- src/Modules/Content/ContentController.php
+++ src/Modules/Content/ContentController.php
@@ -145,6 +145,7 @@ class ContentController extends FoodsharingController
     public function releaseNotes(): Response
     {
         $releaseIds = [
+            '2024-12',
             '2024-08',
             '2024-04',
             '2024-01',
