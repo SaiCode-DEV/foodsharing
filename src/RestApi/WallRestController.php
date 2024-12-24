e6334929acdcd92e604decbe9d288911293ff0b9
Christian Walgenbach <foodsharing@walgeo.de>
2024-12-24 09:19:52 +0000

    rework WallRestController.php

--- src/RestApi/WallRestController.php
+++ src/RestApi/WallRestController.php
@@ -11,13 +11,12 @@ use Foodsharing\Permissions\WallPostPermissions;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use Nelmio\ApiDocBundle\Annotation\Model;
 use OpenApi\Attributes as OA;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
+use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
-use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 #[OA\Tag(name: 'wall')]
 class WallRestController extends AbstractFoodsharingRestController
@@ -28,17 +27,24 @@ class WallRestController extends AbstractFoodsharingRestController
         private readonly WallPostPermissions $wallPostPermissions,
         private readonly WallPostTransactions $wallPostTransactions,
     ) {
+        parent::__construct($session);
     }
 
-    /**
-     * Get posts of a wall.
-     */
+    #[OA\Get(summary: 'Get posts of a wall.')]
     #[Rest\Get('wall/{target}/{targetId}', requirements: ['target' => '\w+', 'targetId' => '\d+'])]
-    #[OA\Response(response: Response::HTTP_OK, description: 'Success', content: new OA\JsonContent(type: 'object', properties: [
+    #[OA\Response(response: Response::HTTP_OK, description: 'Success', content: new OA\JsonContent(properties: [
         new OA\Property(property: 'posts', type: 'array', items: new OA\Items(ref: new Model(type: WallPost::class))),
-        new OA\Property(property: 'mayPost', type: 'boolean', description: 'Whether the user is permitted to post to this wall'),
-        new OA\Property(property: 'mayDelete', type: 'boolean', description: 'whether the user is permitted to delete all posts on this wall'),
-    ]))]
+        new OA\Property(
+            property: 'mayPost',
+            description: 'Whether the user is permitted to post to this wall',
+            type: 'boolean'
+        ),
+        new OA\Property(
+            property: 'mayDelete',
+            description: 'whether the user is permitted to delete all posts on this wall',
+            type: 'boolean'
+        ),
+    ], type: 'object'))]
     #[OA\Response(response: Response::HTTP_FORBIDDEN, description: 'Not permitted to read this wall')]
     public function getPosts(
         string $target,
@@ -58,31 +64,21 @@ class WallRestController extends AbstractFoodsharingRestController
             'mayDelete' => $this->wallPostPermissions->mayDeleteWall($wallType, $targetId)
         ];
 
-        return $this->handleView($this->view($response, Response::HTTP_OK));
+        return $this->respondOK($response);
     }
 
-    /**
-     * Add a post to a wall.
-     */
+    #[OA\Post(summary: 'Add a post to a wall.')]
     #[Rest\Post('wall/{target}/{targetId}', requirements: ['target' => '\w+', 'targetId' => '\d+'])]
-    #[OA\RequestBody(content: new Model(type: WallPost::class),
-        description: 'Only properties `body` and `pictures` are used to create the new post, others may be ommitted.')]
-    #[ParamConverter('wallPost', class: 'Foodsharing\Modules\WallPost\DTO\WallPost', converter: 'fos_rest.request_body')]
     #[OA\Response(response: Response::HTTP_OK, description: 'Success', content: new Model(type: WallPost::class))]
     #[OA\Response(response: Response::HTTP_UNAUTHORIZED, description: 'Not logged in')]
     #[OA\Response(response: Response::HTTP_FORBIDDEN, description: 'Not permitted to post to this wall')]
     #[OA\Response(response: Response::HTTP_BAD_REQUEST, description: 'Invalid post data')]
-    public function addPost(string $target, int $targetId, WallPost $wallPost, ValidatorInterface $validator): Response
+    public function addPost(string $target, int $targetId, #[MapRequestPayload] WallPost $wallPost): Response
     {
         $this->assertLoggedIn();
         $wallType = $this->parseWallType($target);
         if (!$this->wallPostPermissions->mayWriteWall($wallType, $targetId)) {
             throw new AccessDeniedHttpException();
-        }
-        $errors = $validator->validate($wallPost);
-        if ($errors->count() > 0) {
-            $firstError = $errors->get(0);
-            throw new BadRequestHttpException(json_encode(['field' => $firstError->getPropertyPath(), 'message' => $firstError->getMessage()]));
         }
         if (!($wallPost->body || $wallPost->pictures)) {
             throw new BadRequestHttpException('Post cannot be empty');
@@ -90,12 +86,10 @@ class WallRestController extends AbstractFoodsharingRestController
 
         $post = $this->wallPostTransactions->addPost($wallPost, $wallType, $targetId);
 
-        return $this->handleView($this->view($post, Response::HTTP_OK));
+        return $this->respondOK($post);
     }
 
-    /**
-     * Delete a post from a wall.
-     */
+    #[OA\Delete(summary: 'Delete a post from a wall.')]
     #[Rest\Delete('wall/{target}/{targetId}/{postId}', requirements: ['target' => '\w+', 'targetId' => '\d+', 'postId' => '\d+'])]
     #[OA\Response(response: Response::HTTP_OK, description: 'Success')]
     #[OA\Response(response: Response::HTTP_UNAUTHORIZED, description: 'Not logged in')]
@@ -114,7 +108,7 @@ class WallRestController extends AbstractFoodsharingRestController
 
         $this->wallPostTransactions->deletePost($postId, $wallType, $targetId);
 
-        return $this->handleView($this->view(null, Response::HTTP_OK));
+        return $this->respondOK();
     }
 
     private function parseWallType(string $target): WallType
