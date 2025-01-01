4a10e7065a59ac5047997ff8cc55b5f5643c8aff
Christian Walgenbach <foodsharing@walgeo.de>
2025-01-01 17:56:27 +0000

    rework SupportPageRestController

--- src/RestApi/SupportPageRestController.php
+++ src/RestApi/SupportPageRestController.php
@@ -5,15 +5,14 @@ namespace Foodsharing\RestApi;
 use Foodsharing\Lib\Session;
 use Foodsharing\Modules\SupportPage\SupportPageTransactions;
 use Foodsharing\RestApi\Models\SupportPage\TicketModel;
-use FOS\RestBundle\Controller\Annotations as Rest;
 use OpenApi\Attributes\Post;
 use OpenApi\Attributes\Response;
 use OpenApi\Attributes\Tag;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response as HttpResponse;
+use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
 use Symfony\Component\RateLimiter\RateLimiterFactory;
-use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Component\Routing\Annotation\Route;
 
 #[Tag('support')]
 final class SupportPageRestController extends AbstractFoodsharingRestController
@@ -26,16 +25,16 @@ final class SupportPageRestController extends AbstractFoodsharingRestController
     }
 
     #[Post(summary: 'Creates a new support ticket')]
-    #[Rest\Post(path: 'support/ticket')]
+    #[Route('/support/ticket', methods: ['POST'])]
     #[Response(response: HttpResponse::HTTP_OK, description: 'Successful')]
     #[Response(response: HttpResponse::HTTP_BAD_REQUEST, description: 'Invalid data')]
     #[Response(response: HttpResponse::HTTP_SERVICE_UNAVAILABLE, description: 'Support API is not available')]
-    #[ParamConverter('ticketModel', class: TicketModel::class, converter: 'fos_rest.request_body')]
-    public function createTicket(TicketModel $ticketModel, ValidatorInterface $validator, Request $request,
-        RateLimiterFactory $supportTicketLimiter): HttpResponse
-    {
+    public function createTicket(
+        #[MapRequestPayload] TicketModel $ticketModel,
+        Request $request,
+        RateLimiterFactory $supportTicketLimiter
+    ): HttpResponse {
         $this->checkRateLimit($request, $supportTicketLimiter);
-        $this->assertThereAreNoValidationErrors($validator, $ticketModel);
 
         $ticketId = $this->supportPageTransactions->createTicket($ticketModel);
 
