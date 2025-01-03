dee964fc1e17c4ed6da6ca0e0704a40860de4d05
Christian Walgenbach <foodsharing@walgeo.de>
2025-01-03 20:22:08 +0100

    Code style for "Remove body content in content table for dontation modal"

--- migrations/20250103190212_delete_body_in_column_donation_modal_for_table_fs_content.php
+++ migrations/20250103190212_delete_body_in_column_donation_modal_for_table_fs_content.php
@@ -6,7 +6,6 @@ use Phinx\Migration\AbstractMigration;
 
 final class DeleteBodyInColumnDonationModalForTableFsContent extends AbstractMigration
 {
-
     public function change()
     {
         $this->execute("UPDATE `fs_content` SET `body` = '' WHERE `id` = 1");
