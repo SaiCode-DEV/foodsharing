275fd21098b8c88fa8f9e503126e78c6ecbda953
DL6ER <dl6er@dl6er.de>
2025-05-05 15:36:32 +0000

    Fix error 500 when trying to accept an invitation for an event with incomplete address

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -72,6 +72,7 @@
 - Prevent sleeping mode to be submitted with a too long explanation !4126 @alex.simm
 - Fixed the permissions to read blog posts #2372 !4145 @alex.simm
 - Reply with 404 when queriyng stores for non-numeric regionIds in the API #2389 !4174 @DL6ER
+- Fix error 500 when trying to accept an invitation for an event with incomplete address #2375 !4154 @DL6ER
 
 ## Refactoring
 - Get rid of xhr completely !3826 !3933 @chriswalg
--- src/Modules/Core/DTO/Address.php
+++ src/Modules/Core/DTO/Address.php
@@ -28,9 +28,9 @@ class Address
     public static function createFromArray(array $query_result): Address
     {
         $obj = new Address();
-        $obj->street = $query_result['street'];
-        $obj->postalCode = $query_result['postalCode'];
-        $obj->city = $query_result['city'];
+        $obj->street = $query_result['street'] ?? '';
+        $obj->postalCode = $query_result['postalCode'] ?? '';
+        $obj->city = $query_result['city'] ?? '';
 
         return $obj;
     }
