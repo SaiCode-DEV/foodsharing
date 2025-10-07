3caa734126dfed3f51750e55eea7e7b7eea0ebc6
DL6ER <dl6er@dl6er.de>
2025-10-07 12:10:16 +0000

    Ignore error 403 when requesting the wall page as it is expected to be not allowed for jumpers

--- client/src/api/wall.js
+++ client/src/api/wall.js
@@ -1,7 +1,12 @@
 import { get, post, remove } from './base'
 
 export function getWallPosts (target, targetId, limit, offset = 0) {
-  return get(`/wall/${target}/${targetId}?limit=${limit}&offset=${offset}`)
+  // We want to load the wall posts early on in stores, *before* we know if the
+  // user is allowed to see them (jumpers aren't). Hence, the API may respond
+  // with 403 errors that we need to ignore here. See
+  // https://beta.foodsharing.de/region?bid=741&sub=forum&tid=301732&pid=1788926
+  // and my Dominik's reply to this for reference.
+  return get(`/wall/${target}/${targetId}?limit=${limit}&offset=${offset}`, { skipErrorNotificationFor: [403] })
 }
 
 export function addPost (target, targetId, body, pictures) {
