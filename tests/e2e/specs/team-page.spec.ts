47241c7330d7c950f8113b09e45460e557401586
Christian Walgenbach <foodsharing@walgeo.de>
2025-12-06 20:14:48 +0100

    migrate team page acceptance tests to Playwright

---
+++ tests/e2e/specs/team-page.spec.ts
@@ -0,0 +1,28 @@
+import { test, expect } from '../helpers/acceptance';
+import { foodsharing } from '../helpers/foodsharing';
+import RegionIDs from '../helpers/constants/Region/RegionIDs';
+
+test.describe('Team Page', () => {
+  let boardMember: any;
+  let administrationMember: any;
+  let alumniMember: any;
+
+  test.beforeEach(async () => {
+    boardMember = await foodsharing.createFoodsaver();
+    await foodsharing.addRegionMember(RegionIDs.TEAM_BOARD_MEMBER, boardMember.id);
+
+    administrationMember = await foodsharing.createFoodsaver();
+    await foodsharing.addRegionMember(RegionIDs.TEAM_ADMINISTRATION_MEMBER, administrationMember.id);
+
+    alumniMember = await foodsharing.createFoodsaver();
+    await foodsharing.addRegionMember(RegionIDs.TEAM_ALUMNI_MEMBER, alumniMember.id);
+  });
+
+  test('lists board, administration, and alumni members', async ({ page }) => {
+    await page.goto('/team');
+
+    await expect(page.locator('body')).toContainText(boardMember.name);
+    await expect(page.locator('body')).toContainText(administrationMember.name);
+    await expect(page.locator('body')).toContainText(alumniMember.name);
+  });
+});
