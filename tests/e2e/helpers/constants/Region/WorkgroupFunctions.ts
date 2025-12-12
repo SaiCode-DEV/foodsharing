a33482518238039d21dfd290994b7a229188d917
Christian Walgenbach <foodsharing@walgeo.de>
2025-12-12 21:22:03 +0000

    Migrate "foodsaver can join another region" test to playwright

---
+++ tests/e2e/helpers/constants/Region/WorkgroupFunctions.ts
@@ -0,0 +1,20 @@
+/**
+ * Equivalent to PHP class /src/Modules/Core/DBConstants/Region/WorkgroupFunction.php
+ */
+enum WorkgroupFunctions {
+  WELCOME = 1,
+  VOTING = 2,
+  FSP = 3,
+  STORES_COORDINATION = 4,
+  REPORT = 5,
+  MEDIATION = 6,
+  ARBITRATION = 7,
+  FSMANAGEMENT = 8,
+  PR = 9,
+  MODERATION = 10,
+  BOARD = 11,
+  ELECTION = 12,
+  RESOURCES = 13,
+}
+
+export default WorkgroupFunctions;
