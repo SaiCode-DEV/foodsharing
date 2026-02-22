accade5d6c5c5233ba41f0444e5464829d10544e
Alex <alexander.simm@posteo.de>
2026-02-22 15:00:14 +0000

    Resolve "AxiosError: Request failed with status code 500"

--- src/Modules/Report/DTO/ProfileWithMail.php
+++ src/Modules/Report/DTO/ProfileWithMail.php
@@ -6,13 +6,13 @@ use Foodsharing\Modules\Foodsaver\Profile;
 
 class ProfileWithMail extends Profile
 {
-    public string $mail;
-    public string $lastName;
+    public ?string $mail;
+    public ?string $lastName;
 
     public function __construct(array $data, string $prefix = '')
     {
         parent::__construct($data, $prefix);
-        $this->mail = $data[$prefix . 'email'];
-        $this->lastName = $data[$prefix . 'last_name'];
+        $this->mail = $data[$prefix . 'email'] ?? null;
+        $this->lastName = $data[$prefix . 'last_name'] ?? null;
     }
 }
