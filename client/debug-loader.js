4dcebaa3fb455835fd6d0f67c101de5b271c8be8
SaiCode <talentix.loopingflieger@gmail.com>
2026-08-11 16:12:54 +0200

    rm debug-loader

--- client/debug-loader.js
+++
@@ -1,7 +0,0 @@
-const loaderUtils = require('loader-utils')
-
-module.exports = function (source) {
-  const options = loaderUtils.getOptions(this) || {}
-  console.log('DEBUG', options.name, '--', this.resourcePath)
-  return source
-}
